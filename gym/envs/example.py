790ede95ac03daf0f1cad38ce18a29c4f4397d7d
mikyneb <mikulas.nebrensky@tum.de>
2022-05-14 14:01:24 +0200

    Updated example

--- gym/envs/example.py
+++ gym/envs/example.py
@@ -1,11 +1,18 @@
 import gym
 env = gym.make("LunarLander-v2")
 observation, info = env.reset(seed=42, return_info=True)
-for _ in range(1000):
-   env.render()
-   action = policy(observation)  # User-defined policy function
-   observation, reward, done, info = env.step(action)
+episodes = 10
+for episode in range(1, episodes + 1):
+    state =env.reset()
+    done = False
+    score = 0
 
-   if done:
-      observation, info = env.reset(return_info=True)
+    while not done:
+        env.render()
+        action = env.action_space.sample()  
+        observation, reward, done, info = env.step(action)
+        score+=reward
+    print('Episode:{} Score:{}'.format(episode, score))
+    ##if done:
+      ##observation, info = env.reset(return_info=True)
 env.close()
